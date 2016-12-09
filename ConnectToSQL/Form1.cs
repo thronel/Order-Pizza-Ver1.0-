c9395163d6acbf7a80073c8a91935c274fa849ce
thronel <default.nhvu@gmail.com>
2016-12-09 08:00:39 +0700

    Initial Project

---
+++ ConnectToSQL/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ConnectToSQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String StrCon = @"Data Source=VU;Initial Catalog=QLBH;Integrated Security=True";
+            SqlConnection Conn = new SqlConnection(StrCon);
+            try
+            {
+                Conn.Open();
+                MessageBox.Show("Success!", "Bravo", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            catch
+            {
+                MessageBox.Show("Failure!", "Sorry", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ QuanLyBanHang/Model/NhanVienMod.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using QuanLyBanHang.Object;
+
+namespace QuanLyBanHang.Model
+{
+    class NhanVienMod
+    {
+        ConnectDatabase con = new ConnectDatabase();
+        SqlCommand cmd = new SqlCommand();
+
+        public DataTable GetData()
+        {
+            DataTable dt = new DataTable();
+            //MaNV, TenNhanVien, GioiTinh, NamSinh, DiaChi, SDT
+            cmd.CommandText = "select MaNV, TenNhanVien, GioiTinh, NamSinh, DiaChi, SDT from NhanVien";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.Connection;
+            try
+            {
+                con.OpenConn();
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                sda.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                string mex = ex.Message;
+                cmd.Dispose();
+                con.CloseConn();
+            }
+            return dt;
+        }
+
+        public bool AddData(NhanVienObj nvObj)
+        {
+            cmd.CommandText = "Insert into NhanVien values ('" + nvObj.MaNhanVien + "',N'" + nvObj.TenNhanVien + "',N'" + nvObj.GioiTinh + "',CONVERT(DATE,'" + nvObj.NamSinh + "',103),N'" + nvObj.DiaChi + "','" + nvObj.DienThoai + "','" + nvObj.MatKhau + "')";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.Connection;
+            try
+            {
+                con.OpenConn();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mex = ex.Message;
+                cmd.Dispose();
+                con.CloseConn();
+            }
+            return false;
+        }
+
+        public bool UpdData(NhanVienObj nvObj)
+        {
+            cmd.CommandText = "Update NhanVien set TenNhanVien =  N'" + nvObj.TenNhanVien + "', GioiTinh = N'" + nvObj.GioiTinh + "', NamSinh = CONVERT(DATE,'" + nvObj.NamSinh + "',103), DiaChi = N'" + nvObj.DiaChi + "',SDT = '" + nvObj.DienThoai + "' Where MaNV = '" + nvObj.MaNhanVien + "'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.Connection;
+            try
+            {
+                con.OpenConn();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mex = ex.Message;
+                cmd.Dispose();
+                con.CloseConn();
+            }
+            return false;
+        }
+
+        public bool UpdMK(NhanVienObj nvObj)
+        {
+            cmd.CommandText = "Update NhanVien set MatKhau ='" + nvObj.MatKhau + "' Where MaNV = '" + nvObj.MaNhanVien + "'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.Connection;
+            try
+            {
+                con.OpenConn();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mex = ex.Message;
+                cmd.Dispose();
+                con.CloseConn();
+            }
+            return false;
+        }
+
+        public bool DelData(string ma)
+        {
+            cmd.CommandText = "Delete NhanVien Where MaNV = '" + ma + "'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.Connection;
+            try
+            {
+                con.OpenConn();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mex = ex.Message;
+                cmd.Dispose();
+                con.CloseConn();
+            }
+            return false;
+        }
+    }
+}
